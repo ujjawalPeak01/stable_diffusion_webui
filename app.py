3afb09e798f32a2ba4be8d9b9f787e738eb37007
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2023-07-10 21:44:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import webui
 from modules.api.api import Api
 from modules.api.models import StableDiffusionTxt2ImgProcessingAPI, StableDiffusionImg2ImgProcessingAPI
@@ -7,6 +8,12 @@ class InferlessPythonModel:
         webui.initialize()
 
     def infer(self, prompt):
+        folder_path = "models/Stable-diffusion/"
+        absolute_path = os.path.abspath(folder_path)
+        folder_contents = os.listdir(absolute_path)
+        print("Folder contents:", flush=True)
+        for item in folder_contents:
+            print(item, flush=True)
         api = Api()
         arg = StableDiffusionTxt2ImgProcessingAPI(prompt=prompt)
         return api.text2imgapi(txt2imgreq=arg)
